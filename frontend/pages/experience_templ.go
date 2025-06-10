fae644f9e7669a9912a4ac88b8269459fad3cf43
Max Shearer <max@shearer.co>
2025-06-10 21:23:47 +0100

    Regenerate templ files

--- frontend/pages/experience_templ.go
+++ frontend/pages/experience_templ.go
@@ -43,7 +43,7 @@ func Experience() templ.Component {
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"experience\"><header class=\"header\"><a href=\"/\" hx-get=\"/\" hx-target=\"#main\" hx-swap=\"outerHTML\" hx-select=\"#main\" hx-push-url=\"true\">&larr;<span class=\"sr-only\">Back</span></a><h1>Experience</h1></header><dl><div><dt><span>2016 &rarr;</span> now: AFEW, London</dt><dd>Co-founder, creative/technical developer</dd></div><div><dt><span>2022 &rarr; 2023:</span> Tradegraft, London</dt><dd>Co-founder, lead developer</dd></div><div><dt><span>2013 &rarr; 2016:</span> Tui media, London</dt><dd>Senior UI developer</dd></div><div><dt><span>2011 &rarr; 2012:</span> Branded3, Leeds</dt><dd>Designer/developer</dd></div><div><dt><span>2007 &rarr; 2010:</span> Leeds Metropolitan University</dt><dd>BSc Multimedia Technology</dd></div><div><dt><span>2006 &rarr; 2007:</span> 2Cs Communications, Ipswich</dt><dd>Web developer</dd></div></dl><h2>Skills</h2><dl><div><dt>Design</dt><dd><ul><li>Figma</li><li>Sketch</li><li>Photoshop</li><li>Illustrator</li><li>Lightroom</li></ul></dd></div><div><dt>Frontend</dt><dd><ul><li>HTML</li><li>CSS</li><li>JavaScript/TypeScript</li><li>Vue.js/Nuxt</li><li>React/Next.js</li><li>Svelte/SvelteKit</li></ul></dd></div><div><dt>Backend</dt><dd><ul><li>Go</li><li>Node.js</li><li>PostgreSQL</li><li>MongoDB</li><li>Firebase</li><li>Redis</li></ul></dd></div><div><dt>Infra</dt><dd><ul><li>Google Cloud:")
+			templ_7745c5c3_Err = templ.WriteWatchModeString(templ_7745c5c3_Buffer, 1)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -56,7 +56,7 @@ func Experience() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<ul><li>Cloud Run</li><li>Cloud Build</li><li>Cloud Functions</li><li>Cloud Storage</li><li>Stripe</li></ul></li></ul></dd></div><div><dt>Mobile</dt><dd><ul><li>React Native</li><li>Expo</li><li>RevenueCat</li></ul></dd></div></dl></div>")
+			templ_7745c5c3_Err = templ.WriteWatchModeString(templ_7745c5c3_Buffer, 2)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
